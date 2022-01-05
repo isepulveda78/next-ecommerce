394ae22b808a35232101d096fb84f97e804ad75c
Sepulveda <sepulvi@gcsnc.com>
2022-01-05 12:44:44 -0500

    Shipping form added

--- utils/Store.js
+++ utils/Store.js
@@ -8,6 +8,9 @@ const initialState = {
     cartItems: Cookies.get('cartItems')
       ? JSON.parse(Cookies.get('cartItems'))
       : [],
+      shippingAddress: Cookies.get('shippingAddress')
+      ? JSON.parse(Cookies.get('shippingAddress'))
+      : {},
   },
   userInfo: Cookies.get('userInfo')
   ? JSON.parse(Cookies.get('userInfo'))
@@ -44,6 +47,8 @@ function reducer(state, action) {
       return {...state, userInfo: action.payload }
     case 'USER_LOGOUT' :
         return {...state, userInfo: null, cart: { cartItems: [] } }
+    case 'SAVE_SHIPPING_ADDRESS':
+        return {...state, cart: {...state.cart, shippingAddress: action.payload }}
     default:
       return state
   }
