f2f395b36e1ab43268bc349e6670169d3ae23736
Sepulveda <sepulvi@gcsnc.com>
2022-01-05 11:58:52 -0500

    user login and registration

--- pages/shipping.js
+++ pages/shipping.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useContext, useEffect } from 'react'
 import { useRouter } from 'next/router'
+import { Store } from '../utils/Store'
 
 export default function Shipping() {
   const router = useRouter()
-  router.push('/login')
-  return <div></div>
+  const { state, dispatch } = useContext(Store)
+  const { userInfo } = state
+  useEffect(()=> {
+    if(!userInfo){
+      router.push('/login?redirect=/shipping')
+    }
+  })
+  return <div>Shipping Page</div>
 }
