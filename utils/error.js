91042fefe68b3fd4e3bb9e0227987ca3b7ca0723
Israel Sepulveda <sepulveda.israel@gmail.com>
2022-05-19 15:22:38 -0400

    Update error.js

--- utils/error.js
+++ utils/error.js
@@ -9,4 +9,4 @@ const onError = async (err, req, res, next) => {
   await db.disconnect()
   res.status(500).send({ message: err.toString() })
 }
-export default { getError, onError }
+export { getError, onError }
