0c7ee1f7a8893ceb8b8e33556904c967e0df090f
Israel Sepulveda <sepulveda.israel@gmail.com>
2022-05-20 13:14:27 -0400

    Update deliver.js

--- pages/api/orders/[id]/deliver.js
+++ pages/api/orders/[id]/deliver.js
@@ -1,25 +1,25 @@
-import nc from 'next-connect';
-import Order from '../../../../models/Order';
-import db from '../../../../utils/db';
-import onError from '../../../../utils/error';
-import { isAuth } from '../../../../utils/auth';
+import nc from 'next-connect'
+import Order from '../../../../models/Order'
+import db from '../../../../utils/db'
+import { onError } from '../../../../utils/error'
+import { isAuth } from '../../../../utils/auth'
 
 const handler = nc({
   onError,
 });
-handler.use(isAuth);
+handler.use(isAuth)
 handler.put(async (req, res) => {
-  await db.connect();
-  const order = await Order.findById(req.query.id);
+  await db.connect()
+  const order = await Order.findById(req.query.id)
   if (order) {
     order.isDelivered = true;
-    order.deliveredAt = Date.now();
-    const deliveredOrder = await order.save();
+    order.deliveredAt = Date.now()
+    const deliveredOrder = await order.save()
     await db.disconnect();
-    res.send({ message: 'order delivered', order: deliveredOrder });
+    res.send({ message: 'order delivered', order: deliveredOrder })
   } else {
-    await db.disconnect();
-    res.status(404).send({ message: 'order not found' });
+    await db.disconnect()
+    res.status(404).send({ message: 'order not found' })
   }
 });
 
