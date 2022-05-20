e82c40879e1f346341f52d32eb12cf63ac936a91
Israel Sepulveda <sepulveda.israel@gmail.com>
2022-05-20 13:31:48 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 module.exports = {
   reactStrictMode: true,
   images: { domains: ['res.cloudinary.com'] },
-    eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
 };
