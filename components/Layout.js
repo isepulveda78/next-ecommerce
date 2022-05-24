8601905ccb6261fc8d938d78bb965651639058c7
Sepulveda <sepulvi@gcsnc.com>
2022-05-24 12:08:46 -0400

    search & categories

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import Head from 'next/head'
 import NextLink from 'next/link'
 import {
@@ -15,17 +15,45 @@ import {
   Button,
   Menu,
   MenuItem,
+  IconButton,
+  Box,
+  List,
+  ListItem,
+  Drawer,
+  Divider,
+  ListItemText,
+  InputBase,
 } from '@material-ui/core'
 import useStyles from '../utils/styles'
 import { Store } from '../utils/Store'
+import { getError } from '../utils/error'
 import Cookies from 'js-cookie'
 import { useState } from 'react'
 import { useRouter } from 'next/router'
-
+import MenuIcon from '@material-ui/icons/Menu'
+import CancelIcon from '@material-ui/icons/Cancel'
+import SearchIcon from '@material-ui/icons/Search'
+import axios from 'axios'
+import { useSnackbar } from 'notistack'
 export default function Layout({ title, description, children }) {
   const router = useRouter()
   const { state, dispatch } = useContext(Store)
   const { darkMode, cart, userInfo } = state
+  const [sidebarVisible, setSidebarVisible] = useState(false)
+  const [categories, setCategories] = useState([])
+  const { enqueueSnackbar } = useSnackbar()
+
+  const fetchCategories = async () => {
+    try {
+      const { data } = await axios.get(`/api/products/categories`)
+      setCategories(data)
+    }catch(err){
+      enqueueSnackbar(getError(err), { variant: 'error' })
+    }
+  }
+  useEffect(()=> {
+    fetchCategories()
+  }, [])
   const theme = createMuiTheme({
     typography: {
       h1: {
@@ -72,6 +100,20 @@ export default function Layout({ title, description, children }) {
     Cookies.remove('cartItems')
     router.push('/')
   }
+  const sidebarOpenHandler = () => {
+    setSidebarVisible(true)
+  }
+  const sidebarCloseHandler = () => {
+    setSidebarVisible(false)
+  }
+  const [query, setQuery] = useState('')
+  const queryChangeHandler = (e) => {
+    setQuery(e.target.value)
+  }
+  const submitHandler = (e) => {
+    e.preventDefault() 
+    router.push(`/search?query=${query}`)
+  }
   return (
     <div>
       <Head>
@@ -81,13 +123,78 @@ export default function Layout({ title, description, children }) {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <AppBar position="static" className={classes.navbar}>
-          <Toolbar>
-            <NextLink href="/" passHref>
+          <Toolbar className={classes.toolbar}>
+            <Box display="flex" alignItems="center">
+              <IconButton
+                edge="start"
+                aria-label="open drawer"
+                onClick={sidebarOpenHandler}
+                className={classes.menuButton}
+              >
+                <MenuIcon className={classes.navbarButton} />
+              </IconButton>
+              <NextLink href="/" passHref>
               <Link>
                 <Typography className={classes.brand}>Next Js E-commerce</Typography>
               </Link>
             </NextLink>
-            <div className={classes.grow}></div>
+            </Box>
+            <Drawer
+              anchor="left"
+              open={sidebarVisible}
+              onClose={sidebarCloseHandler}
+            >
+              <List>
+                <ListItem>
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="space-between"
+                  >
+                    <Typography>Shopping by Category</Typography>
+                    <IconButton 
+                    aria-label="close"
+                    onClick={sidebarCloseHandler}
+                    >
+                      <CancelIcon />
+                    </IconButton>
+                  </Box>
+                </ListItem>
+                <Divider light />
+                {categories.map((category) => (
+                  <Link>
+                    <Typography component="span">
+                      <NextLink 
+                      key={category}
+                      href={`/search?category=${category}`}
+                      passHref>
+                        <ListItem button component="a" onClick={sidebarCloseHandler}>
+                            <ListItemText primary={category}></ListItemText>
+                        </ListItem>
+                      </NextLink>
+                      </Typography>
+                  </Link>
+                )) 
+              }
+              </List>
+            </Drawer>
+              <div className={classes.searchSection}>
+                <form onSubmit={submitHandler} className={classes.searchForm}>
+                  <InputBase
+                    name="query"
+                    className={classes.searchInput}
+                    placeholder="Search Products"
+                    onChange={queryChangeHandler}
+                    />
+                    <IconButton
+                    type="submit"
+                    className={classes.iconButton}
+                    aria-label="search"
+                    >
+                      <SearchIcon />
+                    </IconButton>
+                </form>
+              </div>
             <div>
               <Switch
                 checked={darkMode}
@@ -95,6 +202,7 @@ export default function Layout({ title, description, children }) {
               ></Switch>
               <NextLink href="/cart" passHref>
                 <Link>
+                <Typography component="span">
                   {cart.cartItems.length > 0 ? (
                     <Badge
                       color="secondary"
@@ -105,6 +213,7 @@ export default function Layout({ title, description, children }) {
                   ) : (
                     'Cart'
                   )}
+                  </Typography>
                 </Link>
               </NextLink>
               {userInfo ? (
@@ -145,7 +254,7 @@ export default function Layout({ title, description, children }) {
                 </>
               ) : (
                 <NextLink href="/login" passHref>
-                  <Link>Login</Link>
+                  <Link><Typography component="span">Login</Typography></Link>
                 </NextLink>
               )}
             </div>
--- utils/styles.js
+++ utils/styles.js
@@ -1,6 +1,6 @@
-import { makeStyles } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core'
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   navbar: {
     backgroundColor: '#203040',
     '& a': {
@@ -32,17 +32,75 @@ const useStyles = makeStyles({
     margin: '0 auto',
   },
   navbarButton: {
-    color: '#fff',
+    color: '#ffffff',
     textTransform: 'initial',
   },
-  transparentBackground: {
-    backgroundColor: 'transparent'
+  transparentBackgroud: {
+    backgroundColor: 'transparent',
   },
   error: {
     color: '#f04040',
   },
   fullWidth: {
-    width: '100%'
-  }
-});
-export default useStyles;
+    width: '100%',
+  },
+  reviewForm: {
+    maxWidth: 800,
+    width: '100%',
+  },
+  reviewItem: {
+    marginRight: '1rem',
+    borderRight: '1px #808080 solid',
+    paddingRight: '1rem',
+  },
+  toolbar: {
+    justifyContent: 'space-between',
+  },
+  menuButton: { padding: 0 },
+  mt1: { marginTop: '1rem' },
+  // search
+  searchSection: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  searchForm: {
+    border: '1px solid #ffffff',
+    backgroundColor: '#ffffff',
+    borderRadius: 5,
+  },
+  searchInput: {
+    paddingLeft: 5,
+    color: '#000000',
+    '& ::placeholder': {
+      color: '#606060',
+    },
+  },
+  iconButton: {
+    backgroundColor: '#f8c040',
+    padding: 5,
+    borderRadius: '0 5px 5px 0',
+    '& span': {
+      color: '#000000',
+    },
+  },
+  sort: {
+    marginRight: 5,
+  },
+
+  fullContainer: { height: '100vh' },
+  mapInputBox: {
+    position: 'absolute',
+    display: 'flex',
+    left: 0,
+    right: 0,
+    margin: '10px auto',
+    width: 300,
+    height: 40,
+    '& input': {
+      width: 250,
+    },
+  },
+}))
+export default useStyles
